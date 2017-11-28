373f173b5acff775005edd2cbfa0b32fef517301
Emerson Victor <evfl@cin.ufpe.br>
2017-11-28 18:10:42 -0300

    Initial commit

---
+++ caminhamentoTeste.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Universidade Federal de Pernambuco (UFPE) (http://www.ufpe.br)
+Centro de Informática (CIn) (http://www.cin.ufpe.br)
+Graduando em Sistemas de Informação
+IF971 - Estatística para Computação
+
+---Autores---
+Nome completo (iniciais)
+Nome completo (iniciais)
+Nome completo (iniciais)
+Nome completo (iniciais)
+Emerson Victor Ferreira da Luz (evfl)
+Nome completo (iniciais)
+
+Emails:  {iniciais, iniciais, iniciais, iniciais, evfl, iniciais}@cin.ufpe.br
+Data:   2017-11-28
+'''
